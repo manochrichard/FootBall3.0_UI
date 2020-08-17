60e61aa0c42bc49ab52177f8427ec795b0004651
manochrichard <manochrichard@gmail.com>
2020-08-17 19:39:23 -0400

    initial commit

---
+++ app/src/main/java/com/manoch/football3/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.manoch.football3;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Intent login = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(login);
+    }
+}
